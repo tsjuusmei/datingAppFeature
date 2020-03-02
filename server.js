8591076aa42d9e5cb276a3ffe06f33af15816979
Anna Boomsma <Anna@MacBookie-van-Anna.local>
2020-03-02 14:25:43 +0100

    Ik heb templates toegevoegd en mn front-end bijgewerkt

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
+
+const data = {
+    persoon : 'Anna',
+    leeftijd : 20,
+    thuis : 'Amsterdam',
+}
+
+
+
+
+
+
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
-app.get('/', (req, res) => res.send('Hello world!'))
+app.use('/static',express.static('static'))
+
+app.set('view engine' , 'ejs')
+
+app.get('/',(req,res) => res.render('index.ejs',{data: data}))
+
+// app.get('/', (req, res) => res.sendfile(path.join(__dirname + '/static/index.html')))
 app.get('/about',(req, res) => res.send('De about pagina bitches'))
+app.get('/contact',(req, res) => res.send('De contact pagina'))
 
-app.listen(port,() => console.log('Example app listening on port ${port}!'))
+app.listen(port,() => console.log('Example app listening on port' + port))
