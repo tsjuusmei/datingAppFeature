f8dcf31dd56d284bc4314d65a28a6663973a1565
annaboomsma <boomsma.anna@gmail.com>
2020-03-31 13:59:12 +0200

    Worked on the server and sessions

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const app = express()
 const port = 3000
 const path = require('path')
 const mongo = require('mongodb')
+const session = require('express-session')
+
+const TWO_HOURS = 1000 * 60 * 60 * 2
 
 require('dotenv').config()
+
 
 // THIS IS THE CODE FOR THE CONNECTION WITH THE DATABASE
 let db = null
@@ -26,42 +30,62 @@ mongo.MongoClient.connect(url, { useUnifiedTopology: true }, function(err, clien
 
 app.use('/static',express.static('static'))
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(session({
+    name: process.env.SESS_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESS_SECRET,
+    cookie: {
+        maxAge: TWO_HOURS,
+        sameSite: true
+
+
+    }
+}))
 
 app.set('view engine' , 'ejs')
 
-
-app.get('/', people)
+app.get('/', home)
+app.get('/results', people)
 app.get('/filter', filters)
 app.get('/static', (req, res) => res.sendfile(path.join(__dirname + '/static/index.html')))
-app.get('/about',(req, res) => res.send('De about pagina bitches'))
+app.get('/login', login)
 app.get('/contact',(req, res) => res.send('De contact pagina'))
 
-app.post('/', add)
+app.post('/results', filter)
+app.post('/login', loginpost)
 
+function home(req,res){
+    console.log(req.session)
+    let { userId } = req.session
+    if(userId = null){
+        res.render('home.ejs')
+    } else{
+        res.render('homeSecond.ejs')
+    }
+    
+}
 
 function filters(req,res){
     res.render('filter.ejs')
 }
 
-function add(req,res){
-    // let genderFilter = req.body.gender
-    let sexualityFilter = req.body.sexuality
-
-    db.collection('datingapp').find({sexuality: sexualityFilter}).toArray(done)
-        function done(err, data){
-            if (err){
-                next(err)
-            } else {
-                console.log(data);
-                res.render('index.ejs', {data: data})
-            }
-        }
+function login(req,res){
+    res.render('login.ejs')
 }
 
-// const genderChecked = document.querySelector('input[name=gender]:checked')
-// const sexualityChecked = document.querySelector('input[name=sexuality]:checked')
-
-
+function loginpost(req,res){
+    const email = res.body.email
+    const password = res.body.password
+    if (email && password){
+        const user = db.collection('datingapp').find(user => user.email === email && user.password === password)
+        if (user){
+            req.session.userId = data._id
+            return res.redirect('/results')
+        }
+    }
+    res.redirect('/login')
+}
 
 
 function people(req, res, next){
@@ -74,6 +98,23 @@ function people(req, res, next){
                 res.render('index.ejs', {data: data})
             }
         }
+}
+
+function filter(req,res){
+    
+    let sexualityFilter = req.body.sexuality
+    let genderFilter = req.body.gender
+
+    db.collection('datingapp').find({gender: genderFilter, sexuality: sexualityFilter}).toArray(done)
+        function done(err, data){
+            if (err){
+                next(err)
+            } else {
+                console.log(data);
+                res.render('index.ejs', {data: data})
+            }
+        }
+     
 }
 
 
