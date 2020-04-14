f33bc5b9fb7552f201ff8854128cba5d9927930b
partychickenking <partychickenking@gmail.com>
2020-04-14 14:44:59 +0200

    Finished uploading images to database

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
-
+const helmet = require('helmet');
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongo = require("mongodb");
 const ObjectID = mongo.ObjectID;
 const session = require("express-session");
 const bcrypt = require('bcrypt')
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const rateLimit = require('express-rate-limit')
+const multer = require('multer')
+const fs = require('fs')
+const path = require('path')
+
 require("dotenv").config();
 
-
-
 const app = express();
+
+//Set sotage engine
+const storage = multer.diskStorage({
+  destination: './static/images/',
+  filename: function(req, file, cb){
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+
+const upload = multer({
+  storage: storage
+})
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 300, // limit each IP to 300 requests per windowMs
+  message: 'Too many requests sent from this IP, please try again after 15 minutes'
+});
 
 const port = 3000;
 
@@ -34,9 +52,11 @@ mongo.MongoClient.connect(url, { useUnifiedTopology: true }, function (
 
 // THIS IS WHERE THE CODE FOR THE DATABASE ENDS
 
-
+app.use(helmet())
 app.use("/static", express.static("static"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(limiter);
 app.use(
   session({
     name: process.env.SESSION_NAME,
@@ -54,32 +74,43 @@ app.set("view engine", "ejs");
 
 app.get("/", home);
 app.get("/results", results);
-app.get('/register', register)
+app.get('/register', register);
 app.get("/filter", filters);
 app.get("/login", login);
-app.get("/profile", profile)
+app.get("/profile", profile);
+app.get("/likes", likes)
 
 app.post("/results", filter);
 app.post("/login", loginpost);
-app.post('/register', registerpost)
+app.post("/like", likepost);
+app.post('/register', upload.single('myImage'), registerpost)
 app.post("/profile", profilepost)
+app.post('/logout', logoutpost)
 
 function home(req, res) {
   let { userId } = req.session;
-  if ((userId = null)) {
+  if (!userId) {
     res.render("home.ejs");
   } else {
-    res.render("homeSecond.ejs");
+    res.redirect('/results');
   }
 }
 
 function results(req, res, next) {
-  db.collection("users").find({}).toArray(done);
+  db.collection('users').find({
+    // This is where we find the userId and the gender & sexuality they want to filter on and we filter the rest of the people with the .find
+    $and: [
+      { firstName: { $ne: req.session.user.firstName } },
+      { gender: req.session.user.filter['gender'] },
+      { sexuality: req.session.user.filter['sexuality'] }
+    ]
+  }).toArray(done)
   function done(err, data) {
     if (err) {
-      next(err);
+      next(err)
     } else {
-      res.render("index.ejs", { data: data });
+      // 
+      res.render('visitors.ejs', { data: data })
     }
   }
 }
@@ -96,52 +127,24 @@ function login(req, res) {
   res.render("login.ejs");
 }
 
+
 function profile(req, res) {
-  // In this function we use the data from the current userId aka the session 
-  res.render('profile.ejs', { data: req.session.userId })
+  // In this function we use the data from the current user aka the session 
+  res.render('profile.ejs', { data: req.session.user })
 }
 
-const storage = multer.diskStorage({
-  destination: function(req, file, cb){
-    cb(null, './static/images')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-  }
-})
-
-const upload = multer({
-  storage: storage
-})
-
-//Configuring the image upload to the database
-app.post('/uploadImage', upload.single('myImage'), (req, res, next) => {
-  const img = fs.readFileSync(req.file.path)
-
-  const encode_image = img.toString('base64')
-
-  // Define a JSON Object for the image
-  const finalImg = {
-    contentType: req.file.mimetype,
-    path: req.file.path,
-    image: new Buffer(encode_image, 'base64')
-  }
-
-  //insert the image to the database
-  db.collection('users').insertOne(finalImg, (err, result) => {
-    console.log(result)
-
-    if(err) return console.log(err)
-
-    console.log('Saved to database')
-
-    res.contentType(finalImg.contentType)
-
-    res.redirect('login')
-  })
-
-  
-})
+async function likes(req, res) {
+  const user = req.session.user;
+  const promises = []; // create an array to store promises
+  user.likedBy.forEach((likerId) => {
+    // make a foreach for each like in likedBy array in db
+    promises.push(
+      db.collection("users").findOne({ _id: new ObjectID(likerId) })
+    ); // For each like in the likedBy array, get the corresponding user from the database and push it as a promise to the promises[]
+  });
+  const likes = await Promise.all(promises);
+  res.render('likes', { likes: likes, user })
+}
 
 async function loginpost(req, res) {
 
@@ -152,7 +155,7 @@ async function loginpost(req, res) {
   }
   try {
     if (await bcrypt.compare(req.body.password, user.password)) {
-      const data = req.session.userId;
+      req.session.user = user
       res.redirect('/results')
     } else {
       res.send('Login failed')
@@ -163,13 +166,25 @@ async function loginpost(req, res) {
 }
 
 // In this function we make sure the user can update it's haircolor and this wil be changed in the database
-function profilepost(req, res) {
+
+async function profilepost(req, res) {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
   db.collection('users').updateOne(
     // First we find the userId aka the session and then we update the haircolor with the input from the user
-    { firstName: req.session.userId.firstName },
-    { $set: { hair: req.body.hair } })
+    { firstName: req.session.user.firstName },
+    {
+      $set: {
+        email: req.body.email,
+        password: hashedPassword,
+        age: req.body.age,
+        hair: req.body.hair,
+        gender: req.body.gender,
+        sexuality: req.body.sexuality
+      }
+    })
 
-  db.collection('users').findOne({ firstName: req.session.userId.firstName }, done)
+  db.collection('users').findOne({ firstName: req.session.user.firstName }, done)
   function done(err, data) {
     if (err) {
       next(err)
@@ -181,17 +196,18 @@ function profilepost(req, res) {
 }
 
 function filter(req, res) {
-  let sexualityFilter = req.body.sexuality;
-  let genderFilter = req.body.gender;
-
-  db.collection("users")
-    .find({ gender: genderFilter, sexuality: sexualityFilter })
-    .toArray(done);
+  db.collection('users').updateOne(
+    { firstName: req.session.user.firstName },
+    { $set: { filter: req.body } })
+  // This is where we find the userId aka the session so we update the preferences for the right user     
+  db.collection('users').findOne({ firstName: req.session.user.firstName }, done)
   function done(err, data) {
     if (err) {
-      next(err);
+      next(err)
     } else {
-      res.render("index.ejs", { data: data });
+      // This is where we redirect the user to the list of people with the filters on
+      req.session.user = data
+      res.redirect('/results')
     }
   }
 }
@@ -199,7 +215,7 @@ function filter(req, res) {
 async function registerpost(req, res, next) {
 
   const hashedPassword = await bcrypt.hash(req.body.password, 10)
-
+  
   db.collection('users').insertOne({
     firstName: req.body.firstname,
     lastName: req.body.lastname,
@@ -211,7 +227,8 @@ async function registerpost(req, res, next) {
     sexuality: req.body.sexuality,
     filter: { gender: "", sexuality: "" },
     visitedBy: [""],
-    likedBy: [""]
+    likedBy: [""],
+    image: req.file ? req.file.filename : null
   }, done)
 
   function done(err, data) {
@@ -223,4 +240,41 @@ async function registerpost(req, res, next) {
   }
 }
 
-app.listen(port, () => console.log("Example app listening on port" + port));
+async function likepost(req, res) {
+  const id = req.body.id;
+  const likedUser = await db
+    .collection("users")
+    .findOne({ _id: ObjectID(id) });
+  if (likedUser.likedBy.includes(req.session.user._id)) {
+    await db
+      .collection("users")
+      .updateOne(
+        { _id: ObjectID(id) },
+        { $pull: { likedBy: req.session.user._id } }
+      );
+    res.sendStatus(201);
+  } else {
+    await db
+      .collection("users")
+      .updateOne(
+        { _id: ObjectID(id) },
+        { $push: { likedBy: req.session.user._id } }
+      );
+    res.sendStatus(200);
+  }
+}
+
+function logoutpost(req, res) {
+  // This is where we destroy the session
+  req.session.destroy(err => {
+    if (err) {
+      return res.redirect('/results')
+    } else {
+      // This is where we clear the cookie and we redirect the user to the homepage
+      res.clearCookie(process.env.SESS_NAME)
+      res.redirect('/')
+    }
+  })
+}
+
+app.listen(port, () => console.log("Example app listening on port " + port));
